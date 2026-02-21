759529b88e2e76cc2e2605050186698bc838aadf
Afjal Hossain <abdullahalafjal31@gmail.com>
2026-02-21 16:35:43 +0600

    added add money and cashout condition

--- script/machine.js
+++ script/machine.js
@@ -19,3 +19,19 @@ function setBalance(value){
     const balanceElement=document.getElementById("balance");
     balanceElement.innerText=value;
 }
+
+// machine id > hide all . show id
+function showOnly(id){
+    // console.log("show only cliked");
+    const addMoney = document.getElementById("add-money");
+    const cashout = document.getElementById("cashout");
+    // console.log(`add money - ${addMoney}, cashout - ${cashout}`);
+    // hide all
+    addMoney.classList.add("hidden");
+    cashout.classList.add("hidden");
+
+    // show only id jar ache
+    const selected = document.getElementById(id);
+    selected.classList.remove("hidden");
+
+}
